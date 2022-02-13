f372b03a360557ebb3c034d4c81536ef7ed02ae3
Jason Young <jason@ytechie.com>
2022-02-12 20:11:16 -0600

    Finally got the output I wanted

--- scripts/getcal.py
+++ scripts/getcal.py
@@ -1,7 +1,7 @@
 from time import strftime
 import icalendar
 import recurring_ical_events
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, time, timedelta, tzinfo
 from urllib import request
 import os
 
@@ -11,27 +11,30 @@ if 'ical' not in os.environ:
 
 ical_url = os.environ['ical']
 
-#ical_string = request.urlopen(ical_url).read()
-ical_string = open('cal.ics','rb').read()
+ical_string = request.urlopen(ical_url).read()
+#ical_string = open('cal.ics','rb').read()
 
 calendar = icalendar.Calendar.from_ical(ical_string)
-events = recurring_ical_events.of(calendar).at(date.today() - timedelta(days=2))
+events = recurring_ical_events.of(calendar).at(date.today() - timedelta(days=0))
 
 def eventSort(e):
-    print(type(e))
-    return 4
     s = e["DTSTART"].dt
-    if(s is datetime):
+    o = datetime.max
+    if(type(s) is datetime):
         o = s
-    elif(s is date):
+    elif(type(s) is date):
         o = datetime.combine(s, datetime.min.time())
-    else:
-        o = datetime.max
-    return o.strftime("%Y-%m-d %H:%M")
+        
+    return o.strftime("%Y-%m-%d %H:%M")
 
-for event in events:#.sort(key=eventSort):
-    print(type(event))
+#events.sort(key=eventSort)
+for event in sorted(events, key=eventSort):
     start = event["DTSTART"].dt
     end = event["DTEND"].dt
-    duration = event["DTEND"].dt - event["DTSTART"].dt
-    print("{}-{} {}".format(start.strftime("%I:%M"), end.strftime("%I:%M"), event["summary"]))
+
+    if(type(start) is date):
+        start = datetime.combine(start, datetime.min.time())
+    if(type(end) is date):
+        end = datetime.combine(end, datetime.min.time())
+
+    print("{}-{} {}".format(start.astimezone().strftime("%I:%M"), end.astimezone().strftime("%I:%M"), event["summary"]))
